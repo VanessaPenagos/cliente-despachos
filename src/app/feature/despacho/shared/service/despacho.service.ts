a13482f651737f7829134bb812a79873a293e47b
VanessaPenagos <vanessapenagos19@gmail.com>
2021-03-30 00:38:20 -0500

    Se agregan validaciones a los servicios

--- src/app/feature/despacho/shared/service/despacho.service.ts
+++ src/app/feature/despacho/shared/service/despacho.service.ts
@@ -2,21 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpService } from '@core-service/http.service';
 import { environment } from 'src/environments/environment';
 import { Despacho } from '../model/despacho';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {  Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DespachoService {
 
-  constructor(protected http: HttpService) { }
+  constructor(protected http: HttpService,  protected router: Router) { }
 
   public buscar(id:number) {
-    return this.http.doGet<Despacho>(`${environment.endpoint}/despachos/${id}`, this.http.optsName('consultar despacho'));
+    return this.http.doGet<Despacho>(`${environment.endpoint}/despachos/${id}`, this.http.optsName('consultar despacho')).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal.fire('Error al cargar el desacho ', 'Despacho no encontrado', 'error');
+        return throwError(e);
+      })
+    );
   }
 
   public crear(despacho: Despacho){
     console.log(despacho)
-    return this.http.doPost<Despacho, boolean>(`${environment.endpoint}/despachos`, despacho, this.http.optsName('crear despacho'));
+    return this.http.doPost<Despacho, boolean>(`${environment.endpoint}/despachos`, despacho, this.http.optsName('crear despacho')).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal.fire('Error al crear el despacho', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   public actualizar(despacho: Despacho){
--- src/app/feature/medicamento/shared/service/medicamento.service.ts
+++ src/app/feature/medicamento/shared/service/medicamento.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '@core-service/http.service';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import swal from 'sweetalert2';
 import { Medicamento } from '../model/medicamento'; 
 
 @Injectable()
@@ -9,11 +12,23 @@ export class MedicamentoService {
   constructor(protected http: HttpService) {}
 
   public consultar() {
-    return this.http.doGet<Medicamento[]>(`${environment.endpoint}/medicamentos`, this.http.optsName('consultar medicamentos'));
+    return this.http.doGet<Medicamento[]>(`${environment.endpoint}/medicamentos`, this.http.optsName('consultar medicamentos')).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal.fire('Error al buscar los medicamentos', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   public crear(medicamento: Medicamento){
-    return this.http.doPost<Medicamento, boolean>(`${environment.endpoint}/medicamentos`, medicamento, this.http.optsName('crear medicamentos'));
+    return this.http.doPost<Medicamento, boolean>(`${environment.endpoint}/medicamentos`, medicamento, this.http.optsName('crear medicamentos')).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal.fire('Error al crear el medicamento', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
 }
